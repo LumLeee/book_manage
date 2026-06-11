c94a389e6943ea49dccad902eda77ae684c020ee
tin14204 <tin14204@gmail.com>
2026-06-11 20:19:35 +0700

    thêm frontend (react), pagination, filter

--- book/views.py
+++ book/views.py
@@ -7,6 +7,9 @@ from django.views.decorators.csrf import csrf_exempt
 from .serializers import BookSerializer
 from .validate.validate_create_book import vaidate_create_book
 from rest_framework.decorators import api_view
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
 def get_request_data(request):
@@ -75,3 +78,11 @@ def detail(request, id):
         book.delete()
         return JsonResponse({'message': 'Book deleted successfully'} )
     return JsonResponse({'error': 'Invalid request method'} )
+
+# Viết bằng cách ModelViewSet
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['title', 'author', 'price', 'quantity']
--- book_manage/settings.py
+++ book_manage/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'book.apps.BookConfig',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -140,6 +141,11 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
+
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
 
