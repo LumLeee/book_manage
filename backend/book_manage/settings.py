50e8ac29fe212c45ec3233202e9fb6482cdf43ce
tin14204 <tin14204@gmail.com>
2026-06-18 14:04:02 +0700

    cập nhật csdl dự phòng cho ci

--- backend/book_manage/settings.py
+++ backend/book_manage/settings.py
@@ -85,11 +85,11 @@ WSGI_APPLICATION = 'book_manage.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'NAME': os.environ.get('DB_NAME', 'book_manage'),
+        'USER': os.environ.get('DB_USER', 'root'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', '123123'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '3306'),
     }
 }
 
